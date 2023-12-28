19b7d881f88407a2d6719f615f8b9642116d1957
pacmandoh <pacmandoh@outlook.com>
2023-12-29 07:04:10 +0800

    chore: special index overflow-hidden

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,9 @@ const vditorContent = `
 const style = {
   container: 'fixed overflow-hidden top-0 bottom-0 left-0 right-0 border-[1.5px] dark:border-[1px] border-gray-300 dark:border-gray-700 rounded-lg mt-[calc(var(--header-height)+1rem)] mx-4 sm:mx-8 md:mx-12 xl:mx-20 2xl:mx-24 lg:mx-16 mb-6 lg:mb-8'
 }
+
+onMounted(() => document.body.style.overflow = 'hidden')
+onBeforeUnmount(() => document.body.style.overflow = '')
 </script>
 
 <template>
