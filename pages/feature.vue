1b065b166b7d3674891c51c8f69642fb77aa116b
pacmandoh <pacmandoh@outlook.com>
2024-03-28 17:27:30 +0800

    feat(page): add feature page to introduce pacdown

--- pages/feature.vue
+++ pages/feature.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
-    特性
+    <ULandingHero :title="page.hero.title" :description="page.hero.description" :links="page.hero.links">
+      <template #headline>
+        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
+          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+            <span class="absolute inset-0" aria-hidden="true" />
+          </NuxtLink>
+
+          {{ page.hero.headline.label }}
+
+          <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="ml-1 w-4 h-4 pointer-events-none" />
+        </UBadge>
+      </template>
+
+      <ULandingLogos :title="page.logos.title" align="center">
+        <UIcon v-for="icon in page.logos.icons" :key="icon" :name="icon" class="w-12 h-12 lg:w-16 lg:h-16 flex-shrink-0 text-gray-900 dark:text-white" />
+      </ULandingLogos>
+    </ULandingHero>
+
+    <ULandingSection id="features" :title="page.features.title" :description="page.features.description" :headline="page.features.headline">
+      <UPageGrid>
+        <ULandingCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" />
+      </UPageGrid>
+    </ULandingSection>
   </div>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+
+defineOgImageComponent(
+  'Landing',
+  {
+    title: page.value.title,
+    description: page.value.description
+  }
+)
+</script>
